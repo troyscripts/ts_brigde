58b0eb5f9dd4a770cc1a8769fcf2024ff581d232
Troy <318667625+troyscripts@users.noreply.github.com>
2026-09-18 14:39:28 +0200

    Delete ts_bridge directory

--- ts_bridge/config.lua
+++
@@ -1,8 +0,0 @@
-TSBridgeConfig = {
-    NotificationTitle = 'Troy Scripts',
-    NotificationDuration = 5000,
-    WaypointKey = 'G',
-    InventoryResource = 'ox_inventory', -- client useItem; gelijk houden aan server_config.lua
-    TargetResource = 'ox_target', -- alleen een ox_target-compatibele resource
-    DeadStateKeys = { 'dead', 'isDead', 'laststand' }
-}
--- ts_bridge/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-author 'Troy Scripts'
-description 'Centrale ondersteuningsbridge voor Troy Scripts'
-version '0.0.1(BETA)'
-shared_scripts { '@ox_lib/init.lua', 'config.lua' }
-client_scripts { 'client/main.lua', 'client/keycard_support.lua', 'client/alerts.lua' }
-server_scripts { 'server_config.lua', 'server/framework.lua', 'server/money.lua', 'server/inventory.lua', 'server/billing.lua', 'server/main.lua', 'server/webhooks.lua' }
-dependency 'ox_lib'
