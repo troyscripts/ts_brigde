a81105df787992ad70f1245e6f4a91fec9d62282
Troy <318667625+troyscripts@users.noreply.github.com>
2026-09-18 14:38:46 +0200

    Delete ts_bridge-0.0.2-BETA directory

--- ts_bridge-0.0.2-BETA/ts_bridge/config.lua
+++
@@ -1,9 +0,0 @@
-TSBridgeConfig = {
-    Locale = 'nl', -- Hoofdtaal; teksten staan in locales/nl.lua
-    NotificationTitle = TSL('config_troy_scripts'),
-    NotificationDuration = 5000,
-    WaypointKey = 'G',
-    InventoryResource = 'ox_inventory', -- client useItem; gelijk houden aan server_config.lua
-    TargetResource = 'ox_target', -- alleen een ox_target-compatibele resource
-    DeadStateKeys = { 'dead', 'isDead', 'laststand' }
-}
